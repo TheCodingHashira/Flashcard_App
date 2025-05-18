0a1188711bacbc688af5a5d05f89901756637003
TheCodingHashira <thevivekpatnaik@gmail.com>
2025-05-18 15:46:31 +0530

    Remove unused Box import from Terms.js

--- src/pages/footer/Terms.js
+++ src/pages/footer/Terms.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Box, Paper } from '@mui/material';
+import { Container, Typography, Paper } from '@mui/material';
 
 function Terms() {
   return (
